cip/kiwix-mobile

commit 3baef2fc84750dac1d8ff56056b84e06cbf791cb

android temp workaround for files not installed: push in script, resp. partly in resource file

Qt/main.cpp
@@ -106,8 +106,8 @@ int main(int argc, char *argv[])
     ZimFileWrapper zimFileWrapper;
 
     //zimFileWrapper.openZimFile("../zim/HTML5VideoDemo.zim");
-    zimFileWrapper.openZimFile("C:\\Users\\Christian\\Downloads\\wikipedia_en_wp1_0.8_45000+_12_2010.zim");
-
+    //zimFileWrapper.openZimFile("C:\\Users\\Christian\\Downloads\\wikipedia_en_wp1_0.8_45000+_12_2010.zim");
+    zimFileWrapper.openZimFile("Bryan Adams.zim");
 # ifdef MEEGO_EDITION_HARMATTAN
     view->setSource(QUrl(QString("%1/qml/main_harmattan.qml").arg(Cordova::instance()->workingDir())));
     view->showFullScreen();
@@ -116,6 +116,9 @@ int main(int argc, char *argv[])
 #  if defined(Q_OS_SYMBIAN) || defined(QT_SIMULATOR)
     view->setSource(QUrl("qml/main.qml"));
     view->showFullScreen();
+#  elif defined(Q_OS_ANDROID)
+    //package qml into resources on android, as else not deployed to device.
+    view->setSource(QUrl("qrc:/qml/main.qml"));
 #  else
     view->setSource(QUrl(QString("%1/qml/main.qml").arg(Cordova::instance()->workingDir())));
     view->show();

Qt/WikipediaMobile.pro
@@ -72,6 +72,9 @@ symbian: {
     message("android")
     LIBS += -L../libs/android -lzimlib
     LIBS += -L../libs/android -lliblzma
+
+    RESOURCES += \
+        qml.qrc
 } else {
     message("Other target")
     LIBS *= -L../../zimlib -lzimlib
@@ -178,3 +181,4 @@ OTHER_FILES += \
 
 message(QT: $$QT)
 message(MOBILITY: $$MOBILITY)
+

Qt/pushfiles.bat
@@ -1,0 +1,12 @@
+
+
+adb root
+adb shell "chmod 777 /data/data/org.kde.necessitas.example.WikipediaMobile/files/"
+adb push "c:\users\Christian\Downloads\Bryan Adams.zim" "/data/data/org.kde.necessitas.example.WikipediaMobile/files/Bryan Adams.zim"
+adb push C:\Symbian\Carbide\workspace\kiwixqt\WikipediaMobileQt\assets\www /data/data/org.kde.necessitas.example.WikipediaMobile/files/
+REM adb shell "mkdir /data/data/org.kde.necessitas.example.WikipediaMobile/files/xml"
+REM #adb push C:\Symbian\Carbide\workspace\kiwixqt\WikipediaMobileQt\res\xml\plugins.xml /data/data/org.kde.necessitas.example.WikipediaMobile/files/xml
+adb push C:\Symbian\Carbide\workspace\kiwixqt\WikipediaMobileQt\res\xml\plugins.xml /data/data/org.kde.necessitas.example.WikipediaMobile/files
+
+
+adb shell "ls  /data/data/org.kde.necessitas.example.WikipediaMobile/files/index.html"

Qt/qml.qrc
@@ -1,0 +1,6 @@
+<RCC>
+    <qresource prefix="/">
+        <file>qml/main.qml</file>
+        <file>qml/cordova_wrapper.js</file>
+    </qresource>
+</RCC>

Qt/src/cordova.cpp
@@ -67,6 +67,7 @@ void Cordova::loadFinished( bool ok ) {
 
     // Try to open the plugins configuration
     QFile pluginsXml( xmlDir.filePath("plugins.xml") );
+    qDebug() << "plugins.xml path:"<< pluginsXml.fileName() << ". xmlDir: "+xmlDir.absolutePath();
     if( !pluginsXml.open( QIODevice::ReadOnly | QIODevice::Text ) ) {
         qDebug() << "Error loading plugins config!";
         return;

Qt/src/zimfilewrapper.cpp
@@ -391,7 +391,8 @@ zim::File::const_iterator ZimFileWrapper::getArticleByUrl(QString articleUrl,QCh
     if (articleUrl == "" || articleUrl == "/")
         //FIXME
         //articleUrl = QLatin1String("/A/HTML5 Demo: Video.html");
-        articleUrl = QLatin1String("/A/Wikipedia.html");
+        //articleUrl = QLatin1String("/A/Wikipedia.html");
+        articleUrl = QLatin1String("/A/Bryan Adams");
 
     qDebug() << "Loading article from url: " << articleUrl ;
 

commit 74a3ee2ceb9b8e656bc0ba4ecc1d6f5859887226

android Fix bug that nothing shown after startup. Bug introduced with 3baef2fc84750dac1d8ff56056b84e06cbf791cb (moved qml in resource file). Note that tested with (not checked in) simple main.qml

Qt/main.cpp
@@ -119,6 +119,7 @@ int main(int argc, char *argv[])
 #  elif defined(Q_OS_ANDROID)
     //package qml into resources on android, as else not deployed to device.
     view->setSource(QUrl("qrc:/qml/main.qml"));
+    view->showFullScreen();
 #  else
     view->setSource(QUrl(QString("%1/qml/main.qml").arg(Cordova::instance()->workingDir())));
     view->show();