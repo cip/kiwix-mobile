b8dbacb78b733674dbaab620d9d8044c634ff22c
cip <cip@gmx.at>
2012-06-09 00:27:22 +0200

    zimreply code cleanup

--- Qt/src/zimreply.h
+++ Qt/src/zimreply.h
@@ -20,33 +20,8 @@ public:
     {        
         setRequest(request);
         setOperation(QNetworkAccessManager::GetOperation);
-        setHeader(QNetworkRequest::ContentTypeHeader,QVariant(QLatin1String("text/html")));
         open(ReadOnly|Unbuffered);
         setUrl(request.url());
-        QString pattern = request.url().queryItemValue(QLatin1String("pattern"));
-        Qt::BrushStyle brushStyle = Qt::SolidPattern;
-        if (pattern == QLatin1String("dense"))
-            brushStyle = Qt::Dense2Pattern;
-        else if (pattern == QLatin1String("cross"))
-            brushStyle = Qt::CrossPattern;
-        else if (pattern == QLatin1String("diagonal"))
-            brushStyle = Qt::FDiagPattern;
-        const QString radiusString = request.url().queryItemValue(QLatin1String("radius"));
-        const QString widthString = request.url().queryItemValue(QLatin1String("width"));
-        const QString heightString = request.url().queryItemValue(QLatin1String("height"));
-        const QString colorString = request.url().queryItemValue(QLatin1String("color"));
-        QColor color(colorString);
-        bool ok;
-        int radius = radiusString.toInt(&ok);
-        if (!ok)
-            radius = 5;
-        QSize size;
-        size.setWidth(widthString.toInt(&ok));
-        if (!ok)
-            size.setWidth(100);
-        size.setHeight(heightString.toInt(&ok));
-        if (!ok)
-            size.setHeight(100);
         qDebug() <<"Creating AsynchronousZimReader";
         //TODO: check whether can be optimized reusing thread.
         // (Would require that requests are strictly sequential,
@@ -101,6 +76,7 @@ public:
 public slots:
     void readFromZimFileDone(const QByteArray& data)
     {
+        qDebug() << Q_FUNC_INFO << " url: " << this->url();
         setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("text/html"));
         position = 0;
         buffer = data;
